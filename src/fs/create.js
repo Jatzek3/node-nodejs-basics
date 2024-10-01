99bbe2017fad4816a232f489982e9571b5510689
Jacek Kawalec <jokerchocolate@gmail.com>
2024-10-01 16:01:11 +0200

    create.js solved

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import fs from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+     if (fs.existsSync('./files/fresh.txt')){
+        throw "FS operation failed" 
+
+    } 
+    else {
+        fs.writeFileSync('./files/fresh.txt', 'I am fresh and young',{});
+    }
 };
 
 await create();
