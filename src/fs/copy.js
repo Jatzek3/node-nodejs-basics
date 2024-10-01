b42252c5f8a99ccca0a8689a8dce372c37d5a330
Jacek Kawalec <jokerchocolate@gmail.com>
2024-10-01 16:31:20 +0200

    copy implemented

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,13 @@
+import fs from 'node:fs';
+
 const copy = async () => {
-    // Write your code here 
+    if (fs.existsSync('files_copy/')){
+
+        throw "FS operation failed" 
+    } 
+    else {
+        fs.cpSync('files', 'files_copy/', {recursive: true})
+    }
 };
 
 await copy();
