f0db3621d152d45acacb724dc5891b6c09441880
Jacek Kawalec <jokerchocolate@gmail.com>
2024-10-02 11:13:13 +0200

    addded hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,10 @@
+import {createHash} from 'crypto'
+import fs  from 'fs'
+
 const calculateHash = async () => {
-    // Write your code here 
+    let hash = createHash('sha256');
+    const data = fs.readFileSync('./files/fileToCalculateHashFor.txt', 'utf8');
+    console.log(hash.update(data).digest('hex'))
 };
 
 await calculateHash();
