00cc5a987827e6a08ad286d06946abd84a6fd734
Jacek Kawalec <jokerchocolate@gmail.com>
2024-10-04 17:18:39 +0200

    added. decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import zlib from 'node:zlib';;
+import fs from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+    const unzip = zlib.createUnzip()
+    const input = fs.createReadStream('./files/archive.gz')
+    const out = fs.createWriteStream('./files/fileToCompress.txt')
+
+    input.pipe(unzip).pipe(out)
+
 };
 
 await decompress();
